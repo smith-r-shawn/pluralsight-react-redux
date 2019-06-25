bac31dbb19478e2be0b89f6f3585f20fc9248717
Shawn Smith <shawn067d.smith@libertymutual.com>
2019-06-25 16:44:49 -0400

    updated redux work

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,7 +4,8 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSTION_COMPOSE__ || compose; //add support for redux dev tools
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for redux dev tools
+
   return createStore(
     rootReducer,
     initialState,
